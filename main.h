336039a5e98496384656ab2ff15e1a4d48fae87a
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2022-11-12 18:26:24 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define _HOLBERTON_H_
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <limits.h>
@@ -15,12 +24,12 @@ char *find_command(char *command);
 int compare(char *s1, char *s2);
 int _strcmpdir(char *s1, char *s2);
 int charput(char c);
-
-
-
-
-
-
-
-
+void place(char *str);
+char *str_concat(char *s1, char *s2);
+int lookforslash(char *cmd);
+int compareExit(char *s1, char *s2);
+int compareEnv(char *s1, char *s2);
+void execute_proc(char **cmd);
+char **identify_string(char *parameter);
+void controlC(int sig);
 #endif
